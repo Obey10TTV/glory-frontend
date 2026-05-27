097ce6b583f83eaef9e7d3084040dd69e1587324
Obioma Ajoku <ajokuobioma@live.co.uk>
2026-05-27 14:25:44 -0700

    Add error boundary

--- src/index.js
+++ src/index.js
@@ -5,11 +5,36 @@ import App from './App'
 import { UserProvider } from './context/UserContext'
 import { CartProvider } from './context/CartContext'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false, error: null }
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error }
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+          <h2>Something went wrong</h2>
+          <pre style={{ color: 'red', whiteSpace: 'pre-wrap' }}>
+            {this.state.error?.toString()}
+          </pre>
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <UserProvider>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </UserProvider>
+  <ErrorBoundary>
+    <UserProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </UserProvider>
+  </ErrorBoundary>
 )
