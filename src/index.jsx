c589964d3882186bfe2f75afbb8d1cfd4d115448
Obioma Ajoku <ajokuobioma10@gmail.com>
2026-08-20 18:46:32 -0700

    refactor: isolate phone layout rules

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import './styles/phone-layout.css'
 import { UserProvider } from './context/UserContext'
 import { CartProvider } from './context/CartContext'
 
